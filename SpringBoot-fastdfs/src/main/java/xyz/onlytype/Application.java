f16781da9a5b14408010b3b93d9ea33e6d35e5b4
codetype <m15319631269@163.com>
2022-12-19 21:11:25 +0800

    fastdfs

---
+++ SpringBoot-fastdfs/src/main/java/xyz/onlytype/Application.java
@@ -0,0 +1,16 @@
+package xyz.onlytype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 白也
+ * @date 2022/12/19 9:06 下午
+ * @title
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
