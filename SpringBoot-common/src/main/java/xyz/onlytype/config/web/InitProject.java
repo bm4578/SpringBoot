fbe28cd9c61b99b3cef07634d235d132cb4283e1
codetype <m15319631269@163.com>
2022-12-20 12:14:08 +0800

    架构改为聚合工程

---
+++ SpringBoot-common/src/main/java/xyz/onlytype/config/web/InitProject.java
@@ -0,0 +1,33 @@
+package xyz.onlytype.config.web;
+
+import lombok.Data;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import java.net.InetAddress;
+
+@Validated
+@ConfigurationProperties(prefix = "server")
+@Data
+public class InitProject implements ApplicationRunner{
+
+    private static final Logger log = LoggerFactory.getLogger(WebMvcConfig.class);
+
+    @Max(value = 1288,message = "端口限制，不能超过最大值")
+    @Min(value = 100,message = "端口限制，不能低于最小值")
+    private int port;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        InetAddress localHost = InetAddress.getLocalHost();
+        String url = "http://"+localHost.getHostAddress()+":"+port;
+        log.warn("服务器端口:  " +url);
+        log.warn("接口文档地址:  " +url+"/doc.html");
+    }
+}
--- SpringBoot-fastdfs/src/main/java/xyz/onlytype/Application.java
+++ SpringBoot-fastdfs/src/main/java/xyz/onlytype/Application.java
@@ -2,6 +2,8 @@ package xyz.onlytype;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import xyz.onlytype.config.web.InitProject;
 
 /**
  * @author 白也
@@ -9,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @title
  */
 @SpringBootApplication
+@EnableConfigurationProperties(InitProject.class)
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class,args);
